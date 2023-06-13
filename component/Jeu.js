047c3a6a2441d232298e517d2900e751b60aff7d
Chloe <b.bernard@lyon.ort.asso.fr>
2023-06-13 15:53:52 +0200

    jeu

--- component/Jeu.js
+++ component/Jeu.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, StyleSheet, View, TextInput } from 'react-native';
+import { Text, StyleSheet, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Jeu() {
   const [text, setText] = useState('');
   const [titre, setTitre] = useState('');
   const [tarif, setTarif] = useState('');
   const [categorie, setCategorie] = useState('');
+  const [jeux, setJeux] = useState([]);
+
+  const ajouterJeu = () => {
+    const jeu = {
+      titre: titre,
+      tarif: tarif,
+      categorie: categorie
+    };
+    setJeux([...jeux, jeu]);
+    setTitre('');
+    setTarif('');
+    setCategorie('');
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.ajout}>
         <Text style={styles.textAjout}>Ajout d'un jeu vidéo</Text>
         <View style={styles.cont}>
-          <View>
+          <View style={styles.inputContainer}>
             <Text style={styles.label}>Titre :</Text>
             <TextInput
               style={styles.input}
@@ -22,7 +36,7 @@ export default function Jeu() {
               placeholder="Titre du jeu"
             />
           </View>
-          <View>
+          <View style={styles.inputContainer}>
             <Text style={styles.label}>Tarif :</Text>
             <TextInput
               style={styles.input}
@@ -31,7 +45,7 @@ export default function Jeu() {
               placeholder="Tarif du jeu"
             />
           </View>
-          <View>
+          <View style={styles.inputContainer}>
             <Text style={styles.label}>Catégorie :</Text>
             <TextInput
               style={styles.input}
@@ -43,6 +57,9 @@ export default function Jeu() {
         </View>
         <StatusBar style="auto" />
       </View>
+      <TouchableOpacity style={styles.addButton} onPress={ajouterJeu}>
+        <AntDesign name="plus" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -50,7 +67,7 @@ export default function Jeu() {
 const styles = StyleSheet.create({
   ajout: {
     position: "relative",
-    top: 135
+    top: 140
   },
   textAjout: {
     color: 'white',
@@ -60,11 +77,16 @@ const styles = StyleSheet.create({
   },
   cont: {
     backgroundColor: 'grey',
-    width: 320,
-    height: 200,
+    width: 250,
+    height: 220,
     position: "relative",
     top: 30,
-    borderRadius: 15
+    borderRadius: 15,
+    right: 35
+  },
+  inputContainer: {
+    flex: 1, // Occupe l'espace disponible
+    marginRight: 10
   },
   input: {
     height: 27,
@@ -80,5 +102,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginLeft: 10,
     marginTop: 10,
-  }
+  },
+  addButton: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    width: 45,
+    marginLeft: 230,
+  },
 });
